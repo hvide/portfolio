cdf93ab8b07b5681e51ab2b09edc9c4bd59a18cb
Davide Gilardoni <davide.gilardoni@bso.co>
2021-12-28 16:37:55 +0000

    Update

--- main.py
+++ main.py
@@ -1,9 +1,63 @@
 from helpers import yml_load, jinja2_load
 from pprint import pprint
 import pandas as pd
+import logging
+from datetime import datetime
+import sys, os
+
+__author__ = 'Davide Gilardoni'
+__email__ = 'dade_gila@hotmail.com'
+__version__ = '0.0.1'
+
+# Logging setup
+
+DATE_STAMP = datetime.now().strftime("%d_%m_%y_%H%M%S")
+LOG_LEVEL = logging.INFO
+
+logger = logging.getLogger('logger')
+logger.setLevel(LOG_LEVEL)
+
+screen_formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', "%Y-%m-%d %H:%M:%S")
+screen = logging.StreamHandler(sys.stdout)
+screen.setFormatter(screen_formatter)
+logger.addHandler(screen)
+
+
+class Portfolio:
+    def __init__(self, name, accounts):
+        self.name = name
+        self.accounts = accounts
+
+    def tot_value(self):
+        x = [account.value for account in self.accounts]
+        return sum(x)
+
+    def tot_equity_value(self):
+        x = [account.value for account in self.accounts]
+        return sum(x)
+
+    def equity_percent(self):
+        x = [account.equity_percent() for account in self.accounts]
+        y = sum(x) / len(self.accounts)
+        return y
+
+    def bond_percent(self):
+        x = [account.bond_percent() for account in self.accounts]
+        y = sum(x) / len(self.accounts)
+        return y
+
+    def unallocated_percent(self):
+        return 100 - (self.equity_percent() + self.bond_percent())
+
 
 class Account:
-    def __init__(self, provider, value, funds=[]):
+    def __init__(self, provider, value, funds):
+        """
+        Collection of fund
+        :param provider: Who is providing the fund
+        :param value: Value of the account in £
+        :param funds: list of funds
+        """
         self.provider = provider
         self.value = value
         self.funds = funds
@@ -24,6 +78,17 @@ class Account:
         """
         x = [fund.actual_ofc_value(value) for fund in self.funds]
         return sum(x)
+
+    def equity_percent(self):
+        x = [fund.allocation_percent for fund in self.funds if fund.fund_type == "equity"]
+        return sum(x)
+
+    def bond_percent(self):
+        x = [fund.allocation_percent for fund in self.funds if fund.fund_type == "bond"]
+        return sum(x)
+
+    def unallocated_percent(self):
+        return 100 - (self.equity_percent() + self.bond_percent())
 
 class Fund:
     def __init__(self, isin, name, nav, ofc, fund_type, allocation_percent):
@@ -62,7 +127,6 @@ class Fund:
 
 
 def main():
-
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
     pd.set_option('display.width', 1000)
@@ -70,13 +134,13 @@ def main():
     funds_info_yml = yml_load('funds_info.yml')
     portfolios_yml = yml_load('portfolios.yml')
 
-
-    for portfolio, acc in portfolios_yml.items():
+    for prtf, acc in portfolios_yml.items():
     # for portfolio in portfolios_yml['portfolios']:
         print('########################')
-        print(portfolio)
+        print(prtf)
+        accounts_list = []
         for p, funds in acc.items():
-            portfolio_list = []
+            funds_list = []
             # test = portfolios_yml[p]
             # print("Account name: %s" % p)
             for fund in funds[1]:
@@ -88,23 +152,32 @@ def main():
                                 ofc=funds_info_yml[fund]['ofc'],
                                 fund_type=funds_info_yml[fund]['fund_type'],
                                 allocation_percent=funds[1][fund])
-                portfolio_list.append(fund_obj)
+                funds_list.append(fund_obj)
 
                 # print(fund.__dict__)
                 # print(fund.allocation_value(portfolios_yml['value']))
                 # print(fund.ofc_percent())
                 # print(fund.actual_ofc())
                 # print(fund.actual_ofc_value(portfolios_yml['value']))
-            account_value = funds[0]['value']
-            account = Account(p, account_value, portfolio_list)
 
-            df = pd.DataFrame([p.to_dict(account_value) for p in portfolio_list])
+            account_value = funds[0]['value']
+            account = Account(p, account_value, funds_list)
+            accounts_list.append(account)
+            df = pd.DataFrame([p.to_dict(account_value) for p in funds_list])
             print("Account name: %s" % account.provider)
-            print("Account value: £%s" % account.value)
+            print("Account value: £ %s" % account.value)
             print("tot_actual_ofc: %s" % account.tot_actual_ofc())
-            print("tot_actual_ofc_value: %s" % account.tot_actual_ofc_value(account_value))
-            print(df)
+            print("tot_actual_ofc_value: £ %s" % account.tot_actual_ofc_value(account_value))
+            print("Account Equity: %s  | Bond: %s" % (account.equity_percent(), account.bond_percent()))
+            print("Account unalocated percent: %s" % account.unallocated_percent())
+            # print(df)
+            logger.debug(df)
             print('')
+        portfolio = Portfolio(prtf, accounts_list)
+        print("Portfolio Equity: %s  | Bond: %s" % (portfolio.equity_percent(), portfolio.bond_percent()))
+        print("Portfolio unalocated percent: %s" % portfolio.unallocated_percent())
+        logger.debug(portfolio.tot_value())
+        logger.debug(portfolio.bond_percent())
         print('')
 
 
